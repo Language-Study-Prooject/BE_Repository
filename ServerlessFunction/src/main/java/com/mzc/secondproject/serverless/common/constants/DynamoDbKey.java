a1f54924055ea4ebd4aa6202f8410d9bf2b0e113
ddingjoo <ddingsha9@teambind.co.kr>
2026-01-11 17:28:47 +0900

    feat(dynamodb): GSI3 Sparse Index로 북마크 쿼리 최적화
      - UserWord 모델에 GSI3 필드(gsi3pk, gsi3sk) 추가
      - DynamoDbKey에 GSI3 상수 추가
      - VocabKey에 userBookmarkedPk() 키 빌더 추가
      - UserWordRepository.findBookmarkedWords() FilterExpression -> GSI3 쿼리로 변경
      - UserWordCommandService 북마크 설정 시 GSI3 키 설정/해제
      - template.yaml VocabTable에 GSI3 인덱스 정의 추가
    
      Refs #176

--- ServerlessFunction/src/main/java/com/mzc/secondproject/serverless/common/constants/DynamoDbKey.java
+++ ServerlessFunction/src/main/java/com/mzc/secondproject/serverless/common/constants/DynamoDbKey.java
@@ -13,10 +13,13 @@ public final class DynamoDbKey {
     public static final String GSI1_SK = "GSI1SK";
     public static final String GSI2_PK = "GSI2PK";
     public static final String GSI2_SK = "GSI2SK";
+    public static final String GSI3_PK = "GSI3PK";
+    public static final String GSI3_SK = "GSI3SK";
 
     // Index Names
     public static final String GSI1 = "GSI1";
     public static final String GSI2 = "GSI2";
+    public static final String GSI3 = "GSI3";
 
     // Common Sort Key
     public static final String METADATA = "METADATA";
