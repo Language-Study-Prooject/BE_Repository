9d2d258ee84f7ec4e3c9c627bc77fabdfea54713
hye-inA <hye.ina0130@gmail.com>
2026-01-20 11:02:19 +0900

    feat : OPIc 질문 & 세션 관련 dto 생성

---
+++ ServerlessFunction/src/main/java/com/mzc/secondproject/serverless/domain/opic/dto/response/CreateSessionResponse.java
@@ -0,0 +1,7 @@
+package com.mzc.secondproject.serverless.domain.opic.dto.response;
+
+public record CreateSessionResponse (
+        String sessionId,
+        QuestionResponse firstQuestion,
+        int totalQuestions
+) {}
