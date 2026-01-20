1557ae45830cb7178fca26b2ac5792a9a211b1b0
ddingjoo <ddingsha9@teambind.co.kr>
2026-01-20 15:26:27 +0900

    style: GameConfig 코드 포맷팅

--- ServerlessFunction/src/main/java/com/mzc/secondproject/serverless/domain/chatting/config/GameConfig.java
+++ ServerlessFunction/src/main/java/com/mzc/secondproject/serverless/domain/chatting/config/GameConfig.java
@@ -7,26 +7,26 @@ import com.mzc.secondproject.serverless.common.config.EnvConfig;
  * 환경 변수로 오버라이드 가능
  */
 public final class GameConfig {
-
+	
 	private static final int DEFAULT_TOTAL_ROUNDS = 5;
 	private static final int DEFAULT_ROUND_TIME_LIMIT = 60;
 	private static final long DEFAULT_QUICK_GUESS_THRESHOLD_MS = 5000L;
-
+	
 	private static final int TOTAL_ROUNDS = EnvConfig.getIntOrDefault("GAME_TOTAL_ROUNDS", DEFAULT_TOTAL_ROUNDS);
 	private static final int ROUND_TIME_LIMIT = EnvConfig.getIntOrDefault("GAME_ROUND_TIME_LIMIT", DEFAULT_ROUND_TIME_LIMIT);
 	private static final long QUICK_GUESS_THRESHOLD_MS = EnvConfig.getLongOrDefault("GAME_QUICK_GUESS_THRESHOLD_MS", DEFAULT_QUICK_GUESS_THRESHOLD_MS);
-
+	
 	private GameConfig() {
 	}
-
+	
 	public static int totalRounds() {
 		return TOTAL_ROUNDS;
 	}
-
+	
 	public static int roundTimeLimit() {
 		return ROUND_TIME_LIMIT;
 	}
-
+	
 	public static long quickGuessThresholdMs() {
 		return QUICK_GUESS_THRESHOLD_MS;
 	}
