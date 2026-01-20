c116134435986c1d8ad8e9add13d1b61db81dd7b
ddingjoo <ddingsha9@teambind.co.kr>
2026-01-20 15:27:13 +0900

    style: VocabularyErrorCodeSpec 코드 포맷팅

--- ServerlessFunction/src/test/groovy/com/mzc/secondproject/serverless/domain/vocabulary/exception/VocabularyErrorCodeSpec.groovy
+++ ServerlessFunction/src/test/groovy/com/mzc/secondproject/serverless/domain/vocabulary/exception/VocabularyErrorCodeSpec.groovy
@@ -19,22 +19,22 @@ class VocabularyErrorCodeSpec extends Specification {
         !errorCode.getMessage().isEmpty()
 
         where:
-        errorCode                                    | expectedCode     | expectedStatusCode
-        VocabularyErrorCode.WORD_NOT_FOUND           | "WORD_001"       | 404
-        VocabularyErrorCode.WORD_ALREADY_EXISTS      | "WORD_002"       | 409
-        VocabularyErrorCode.INVALID_WORD_DATA        | "WORD_003"       | 400
-        VocabularyErrorCode.USER_WORD_NOT_FOUND      | "USER_WORD_001"  | 404
-        VocabularyErrorCode.INVALID_DIFFICULTY       | "USER_WORD_002"  | 400
-        VocabularyErrorCode.INVALID_WORD_STATUS      | "USER_WORD_003"  | 400
-        VocabularyErrorCode.DAILY_STUDY_NOT_FOUND    | "STUDY_001"      | 404
-        VocabularyErrorCode.STUDY_LIMIT_EXCEEDED     | "STUDY_002"      | 400
-        VocabularyErrorCode.INVALID_STUDY_LEVEL      | "STUDY_003"      | 400
-        VocabularyErrorCode.INVALID_CATEGORY         | "CATEGORY_001"   | 400
-        VocabularyErrorCode.INVALID_LEVEL            | "LEVEL_001"      | 400
-        VocabularyErrorCode.GROUP_NOT_FOUND          | "GROUP_001"      | 404
-        VocabularyErrorCode.GROUP_ALREADY_EXISTS     | "GROUP_002"      | 409
-        VocabularyErrorCode.TEST_NOT_FOUND           | "TEST_001"       | 404
-        VocabularyErrorCode.NO_WORDS_TO_TEST         | "TEST_002"       | 400
+        errorCode                                 | expectedCode    | expectedStatusCode
+        VocabularyErrorCode.WORD_NOT_FOUND        | "WORD_001"      | 404
+        VocabularyErrorCode.WORD_ALREADY_EXISTS   | "WORD_002"      | 409
+        VocabularyErrorCode.INVALID_WORD_DATA     | "WORD_003"      | 400
+        VocabularyErrorCode.USER_WORD_NOT_FOUND   | "USER_WORD_001" | 404
+        VocabularyErrorCode.INVALID_DIFFICULTY    | "USER_WORD_002" | 400
+        VocabularyErrorCode.INVALID_WORD_STATUS   | "USER_WORD_003" | 400
+        VocabularyErrorCode.DAILY_STUDY_NOT_FOUND | "STUDY_001"     | 404
+        VocabularyErrorCode.STUDY_LIMIT_EXCEEDED  | "STUDY_002"     | 400
+        VocabularyErrorCode.INVALID_STUDY_LEVEL   | "STUDY_003"     | 400
+        VocabularyErrorCode.INVALID_CATEGORY      | "CATEGORY_001"  | 400
+        VocabularyErrorCode.INVALID_LEVEL         | "LEVEL_001"     | 400
+        VocabularyErrorCode.GROUP_NOT_FOUND       | "GROUP_001"     | 404
+        VocabularyErrorCode.GROUP_ALREADY_EXISTS  | "GROUP_002"     | 409
+        VocabularyErrorCode.TEST_NOT_FOUND        | "TEST_001"      | 404
+        VocabularyErrorCode.NO_WORDS_TO_TEST      | "TEST_002"      | 400
     }
 
     def "404 에러 코드들 확인"() {
