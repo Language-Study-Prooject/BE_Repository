86d0c1365ee9292e102d0bcecb7de1a3c9b08960
ddingjoo <ddingsha9@teambind.co.kr>
2026-01-20 15:26:27 +0900

    style: GrammarConfig 코드 포맷팅

--- ServerlessFunction/src/main/java/com/mzc/secondproject/serverless/domain/grammar/config/GrammarConfig.java
+++ ServerlessFunction/src/main/java/com/mzc/secondproject/serverless/domain/grammar/config/GrammarConfig.java
@@ -7,32 +7,32 @@ import com.mzc.secondproject.serverless.common.config.EnvConfig;
  * 환경 변수로 오버라이드 가능
  */
 public final class GrammarConfig {
-
+	
 	private static final int DEFAULT_SESSION_TTL_DAYS = 30;
 	private static final int DEFAULT_MAX_HISTORY_MESSAGES = 10;
 	private static final int DEFAULT_LAST_MESSAGE_MAX_LENGTH = 100;
 	private static final int DEFAULT_MAX_TOKENS = 2048;
-
+	
 	private static final int SESSION_TTL_DAYS = EnvConfig.getIntOrDefault("GRAMMAR_SESSION_TTL_DAYS", DEFAULT_SESSION_TTL_DAYS);
 	private static final int MAX_HISTORY_MESSAGES = EnvConfig.getIntOrDefault("GRAMMAR_MAX_HISTORY_MESSAGES", DEFAULT_MAX_HISTORY_MESSAGES);
 	private static final int LAST_MESSAGE_MAX_LENGTH = EnvConfig.getIntOrDefault("GRAMMAR_LAST_MESSAGE_MAX_LENGTH", DEFAULT_LAST_MESSAGE_MAX_LENGTH);
 	private static final int MAX_TOKENS = EnvConfig.getIntOrDefault("GRAMMAR_MAX_TOKENS", DEFAULT_MAX_TOKENS);
-
+	
 	private GrammarConfig() {
 	}
-
+	
 	public static int sessionTtlDays() {
 		return SESSION_TTL_DAYS;
 	}
-
+	
 	public static int maxHistoryMessages() {
 		return MAX_HISTORY_MESSAGES;
 	}
-
+	
 	public static int lastMessageMaxLength() {
 		return LAST_MESSAGE_MAX_LENGTH;
 	}
-
+	
 	public static int maxTokens() {
 		return MAX_TOKENS;
 	}
